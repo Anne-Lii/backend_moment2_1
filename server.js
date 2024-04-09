ae4b9c1017ff7d630b9790af54f2549edeaefe13
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-04-09 09:52:52 +0200

    lagt till ett skal enligt Malins teoriföreläsningar

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
-const express = require("express");
-const app = express();
-const port = process.env.DB_PORT || 3000;
+const express = require("express");  //include express
+const cors = require("cors");                        
+const app = express();                                              //start express
+const port = 3000; 
+
+app.use(cors());
+app.use(express.json());
 
 //Routing
+//Get workexperience
 app.get("/api", (req, res) => {
     res.json({message: " Välkommen till mitt API"})
 });
 
+app.get("/api/work", (req, res) => {
+    res.json({message: " get work"});
+});
 
+//Add workexperience
+app.post("/api/work", (req, res) => {
+
+    let {
+        companyname,
+        location,
+        jobtitle,
+        description,
+        startdate,
+        enddate        
+    } = req.body;
+
+    if(!companyname || !location || !jobtitle || !description || !startdate || !enddate) {
+       
+        res.status(400).json({error: "Fill in all"});
+        return;
+    }
+
+    let work = {
+        companyname,
+        location,
+        jobtitle,
+        description,
+        startdate,
+        enddate 
+    };
+
+    res.json({message: "Work added", work});
+});
+
+//Update workexperience
+app.put("/api/work/:id", (req, res) => {
+    res.json({message: "Work updated: " + req.params.id});
+});
+
+//Delete workexperience
+app.delete("/api/work/:id", (req, res) => {
+    res.json({message: "Work deleted: " + req.params.id});
+});
+
+//start app
 app.listen(port, () => {
     console.log("Server is running on port: "+ port)
 });
